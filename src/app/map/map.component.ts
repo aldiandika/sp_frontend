e030b38e84a483346ae0f8d4e3fa9ce2b36364c6
aldiandika <aldi.andika@gmail.com>
2019-11-21 10:17:48 +0700

    Tanbah function untuk get data real time

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -16,15 +16,15 @@ import { fromLonLat } from "ol/proj";
 
 import { HttpClient } from "@angular/common/http";
 
-import axios from "axios";
-
 const PUSHER_API_KEY = "6f4176ccff502d8ce53b";
 const PUSHER_CLUSTER = "ap1";
 
 declare var google: any;
-var lat: any;
-var long: any;
+var latitude: any;
+var longitude: any;
 var geolocation;
+var data: any;
+var dataG;
 
 @Component({
   selector: "app-map",
@@ -46,9 +46,10 @@ export class MapComponent implements OnInit {
 
   yes: any;
 
-  data: any;
-  lat: number = -6.921867999999984;
-  long: number = 107.607093;
+  dataMap: any;
+
+  // lat: number = -6.921867999999984;
+  // long: number = 107.607093;
   marker: any;
   lineCoordinates = [];
 
@@ -58,19 +59,75 @@ export class MapComponent implements OnInit {
   // constructor() {}
 
   ngOnInit() {
-    this.launchMap(this.lat, this.long);
-    this.getData();
+    // this.GeoLocMap(this.lat, this.long);
+    this.stayUpdate();
+    this.getDataNlaunch();
+    // this.launchMap();
   }
 
-  getData() {
+  //Function to get data from database and launch map
+  getDataNlaunch() {
     const url = "http://localhost:8000/api/tracklast";
     this.http.get(url).subscribe(res => {
-      this.data = res;
-      console.log(this.data);
+      data = res;
+      latitude = data.t_lat;
+      longitude = data.t_lon;
+
+      this.launchMap();
     });
   }
 
-  launchMap(lat, lng) {
+  //Function to launch map
+  launchMap() {
+    this.source = new OlXYZ({
+      url: "http://tile.osm.org/{z}/{x}/{y}.png"
+    });
+
+    this.layer = new OlTileLayer({
+      source: this.source
+    });
+
+    var view = new OlView({
+      center: fromLonLat([longitude, latitude]),
+      zoom: 6
+    });
+
+    var positionFeature = new Feature({
+      geometry: new Point(fromLonLat([longitude, latitude]))
+    });
+
+    positionFeature.setStyle(
+      new Style({
+        image: new CircleStyle({
+          radius: 6,
+          fill: new Fill({
+            color: "#3399CC"
+          }),
+          stroke: new Stroke({
+            color: "#fff",
+            width: 2
+          })
+        })
+      })
+    );
+
+    this.vectorSource = new VectorSource({
+      features: [positionFeature]
+    });
+
+    this.vectorLayer = new VectorLayer({
+      source: this.vectorSource
+    });
+
+    this.map = new OlMap({
+      target: "map",
+      layers: [this.layer, this.vectorLayer],
+      view: view
+    });
+  }
+
+  //Function to get geo Location data
+  GeoLocMap(lat, lng) {
     this.source = new OlXYZ({
       url: "http://tile.osm.org/{z}/{x}/{y}.png"
     });
@@ -136,95 +193,21 @@ export class MapComponent implements OnInit {
     });
   }
 
-  // this.geolocation = new Geolocation({
-  //   // enableHighAccuracy must be set to true to have the heading value.
-  //   trackingOptions: {
-  //     enableHighAccuracy: true
-  //   },
-  //   projection: this.view.getProjection()
-  // });
+  //function to get data real time
+  stayUpdate() {
+    var echo = new Echo({
+      broadcaster: "pusher",
+      key: PUSHER_API_KEY,
+      cluster: PUSHER_CLUSTER
+    });
+    echo.channel("location").listen("SendLocation", e => {
+      this.dataMap = e.location;
+      console.log(this.dataMap);
+      // this.updateMap(this.dataMap);
+    });
+  }
 
-  // this.geolocation.setTracking(true);
-
-  // this.accuracyFeature = new Feature();
-  // this.geolocation.on("change:accuracyGeometry", function() {
-  //   this.accuracyFeature.setGeometry(this.geolocation.getAccuracyGeometry());
-  // });
-
-  // this.positionFeature = new Feature();
-  // this.positionFeature.setStyle(
-  //   new Style({
-  //     image: new CircleStyle({
-  //       radius: 8,
-  //       fill: new Fill({
-  //         color: "#3399CC"
-  //       }),
-  //       stroke: new Stroke({
-  //         color: "#fff",
-  //         width: 2
-  //       })
-  //     })
-  //   })
-  // );
-
-  // this.geolocation.on("change:position", function() {
-  //   var coordinates = this.geolocation.getPosition();
-  //   this.positionFeature.setGeometry(
-  //     coordinates ? new Point(coordinates) : null
-  //   );
-  // });
-
-  // this.yes = new Feature();
-  // this.yes.setStyle(
-  //   new Style({
-  //     image: new CircleStyle({
-  //       radius: 8,
-  //       fill: new Fill({
-  //         color: "#3399CC"
-  //       }),
-  //       stroke: new Stroke({
-  //         color: "#fff",
-  //         width: 2
-  //       })
-  //     })
-  //   })
-  // );
-
-  // new VectorLayer({
-  //   map: this.map,
-  //   source: new VectorSource({
-  //     features: [this.positionFeature, this.yes, this.accuracyFeature]
-  //   })
-  // });
-
-  // subscribe() {
-  //   var echo = new Echo({
-  //     broadcaster: "pusher",
-  //     key: PUSHER_API_KEY,
-  //     cluster: PUSHER_CLUSTER
-  //   });
-  //   echo.channel("location").listen("SendLocation", e => {
-  //     this.data = e.location;
-  //     this.updateMap(this.data);
-  //   });
-  // }
-
-  // updateMap(data) {
-  //   this.lat = parseFloat(data.lat);
-  //   this.long = parseFloat(data.long);
-
-  //   this.map.setCenter({ lat: this.lat, lng: this.long, alt: 0 });
-  //   this.marker.setPosition({ lat: this.lat, lng: this.long, alt: 0 });
-
-  //   this.lineCoordinates.push(new google.maps.LatLng(this.lat, this.long));
-
-  //   var lineCoordinatesPath = new google.maps.Polyline({
-  //     path: this.lineCoordinates,
-  //     geodesic: true,
-  //     map: this.map,
-  //     strokeColor: "#FF0000",
-  //     strokeOpacity: 1.0,
-  //     strokeWeight: 2
-  //   });
-  // }
+  updateMap(data) {
+    console.log("Update Map");
+  }
 }
