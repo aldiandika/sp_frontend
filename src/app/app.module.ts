393c4ddb4ac62ddeb38a994dd70f3142eb2368e5
koko <makmurkp@gmail.com>
2019-12-11 15:59:10 +0700

    chat v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
@@ -5,7 +6,7 @@ import { AppComponent } from './app.component';
 import { MapComponent, ReportDialog } from './map/map.component';
 import { MapboComponent } from './mapbo/mapbo.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatDialogModule, MatCardModule, MatSliderModule , MatToolbarModule, MatButtonModule, MatGridListModule } from '@angular/material';
+import { MatDialogModule, MatCardModule, MatSliderModule, MatToolbarModule, MatButtonModule, MatGridListModule, MatNavList, MatListModule } from '@angular/material';
 import { DialogContentExampleDialog } from './pop-kom/pop-kom.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PopKomComponent } from './pop-kom/pop-kom.component';
@@ -40,6 +41,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatSidenavModule,
     MatFormFieldModule,
     FlexLayoutModule,
+    MatListModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
